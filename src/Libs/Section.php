7fa183951397640b0984f13213b475bf426a2d6b
dhenfie <fajarsusilo1600@gmail.com>
2023-05-18 14:20:36 +0700

    feat: create block Section class with unit test

--- src/Libs/Section.php
+++ src/Libs/Section.php
@@ -4,5 +4,44 @@ namespace Dhenfie\Nuri\Libs;
 
 class Section
 {
-    //
+    private array $section = [];
+
+    private string $currentSection;
+
+
+    public function create(string $name, string $values) : void
+    {
+        $this->section[$name] = $values;
+    }
+
+    public function start(string $name)
+    {
+        $this->currentSection = $name;
+        ob_start();
+    }
+
+    public function end()
+    {
+        $contents                             = $this->getContents();
+        $this->section[$this->currentSection] = $contents;
+
+        // clean and turn off output buffering
+        ob_end_clean();
+    }
+
+    // public function getSection(string $name): string {
+    //     // retur
+    // }
+
+    public function getContents() : string
+    {
+        $contents = ob_get_contents();
+        return ($contents === false) ? '' : $contents;
+    }
+
+    public function getSection(string $name) : string
+    {
+        $contents = isset($this->section[$name]) ? $this->section[$name] : '';
+        return $contents;
+    }
 }
--- tests/SectionTest.php
+++ tests/SectionTest.php
@@ -1,9 +1,45 @@
 <?php
 
 namespace Dhenfie\Nuri\Tests;
+
+use Dhenfie\Nuri\Libs\Section;
 use PHPUnit\Framework\TestCase;
 
 class SectionTest extends TestCase
 {
-    //
+    private Section $block;
+
+    public function setUp() : void
+    {
+        $this->block = new Section;
+    }
+
+    public function testCreate() : void
+    {
+        self::expectOutputString('hello world !');
+
+        $this->block->create('message', 'hello world !');
+        echo $this->block->getSection('message');
+    }
+
+    public function testStartSection(): void {
+        self::expectOutputString('PHP Template System');
+
+        $this->block->start('message');
+        echo 'PHP Template System';
+        $this->block->end();
+
+        // print message
+        echo $this->block->getSection('message');
+    }
+
+    public function testUndefinedSection(){
+        self::expectOutputString('');
+
+        $this->block->start('message');
+        echo 'test !';
+        $this->block->end();
+
+        echo $this->block->getSection('undefined');
+    }
 }
